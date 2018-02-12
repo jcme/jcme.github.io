73eedc051ab49be49073ee16bfb9df50984387ee
Jamie Holmes <jamie.holmes@tesco.com>
2018-02-12 00:12:06 +0000

    Add 2

---
+++ 2.js
@@ -0,0 +1,44 @@
+
+var to = 'Dan!';
+var gift_url = false;
+var gift_image_url = 'steam.png';
+
+
+
+var nametag = document.getElementById("nametag");
+var present = document.getElementById("present");
+var presentImage = document.getElementById("present-image");
+
+
+function init() {
+
+  var _giftLink,
+      _giftImg;
+
+  if (gift_url) {
+    _giftLink = document.createElement("a");
+    _giftLink.href = gift_url;
+    _giftLink.target = "_blank";
+    presentImage.appendChild(_giftLink);
+  }
+
+  if (gift_image_url) {
+    _giftImg = document.createElement("img");
+    _giftImg.src = gift_image_url;
+    if(_giftLink) {
+      _giftLink.appendChild(_giftImg);
+    } else {
+      presentImage.appendChild(_giftImg);
+    }
+  }
+
+  present.addEventListener("click", function(e){
+    present.classList.toggle("open");
+  }, false);
+
+
+
+  nametag.innerText = to;
+}
+
+init();
